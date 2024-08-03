16faaae1940201e771aba9cb60dad91643928957
alexjuda <alexanderjuda@gmail.com>
2024-08-03 23:32:31 +0200

    impl: read batch

--- src/pulsar_viewer/pulsar/_poller.py
+++ src/pulsar_viewer/pulsar/_poller.py
@@ -1,4 +1,6 @@
+import pulsar
 from ._msg import Message
+from functools import cached_property
 
 
 class PulsarPoller:
@@ -6,6 +8,29 @@ class PulsarPoller:
         self._pulsar_url = pulsar_url
         self._topic_fq = topic_fq
 
+    @cached_property
+    def client(self):
+        return pulsar.Client(service_url=self._pulsar_url)
+
+    @cached_property
+    def reader(self):
+        return self.client.create_reader(
+            topic=self._topic_fq,
+            start_message_id=pulsar.MessageId.earliest,
+        )
+
+    TIMEOUT_IMMEDIATELY = 0
+
     def read_new_batch(self) -> list[Message] | None:
-        ...
-        return None
+        msgs = []
+        while True:
+            try:
+                msg = self.reader.read_next(timeout_millis=1000)
+            except pulsar.Timeout:
+                break
+            msgs.append(msg)
+
+        if msgs:
+            return msgs
+        else:
+            return None

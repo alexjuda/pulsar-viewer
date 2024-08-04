85930d114ffd232abcab7a0892a7a973f5a7eac3
alexjuda <alexanderjuda@gmail.com>
2024-08-04 20:34:39 +0200

    Add faulty prototype of interaction

--- src/pulsar_viewer/toga/main_window/_ctrl.py
+++ src/pulsar_viewer/toga/main_window/_ctrl.py
@@ -8,17 +8,17 @@ class MainWindowCtrl:
     def __init__(self, vm: MainWindowVM):
         self._vm = vm
 
-        switch = toga.Switch(text="Show full message")
+        switch = toga.Switch(text="Show full message", on_change=self._on_switch_change)
         # Wrapping the switch in a box makes the control and the label clumped
         # together instead of stretched to till the container.
-        switch_box = toga.Box(children=[switch])
+        self._switch_box = toga.Box(children=[switch])
 
-        messages_view = toga.Label("Messages\nwill\nshow up here.")
-        message_details_view = toga.Label("This will contain\nthe message at its full length.")
-        split = toga.SplitContainer(content=(messages_view, message_details_view))
+        self._messages_view = toga.Label("Messages\nwill\nshow up here.")
+        self._message_details_view = toga.Label("This will contain\nthe message at its full length.")
+        self._split = toga.SplitContainer(content=(self._messages_view, self._message_details_view))
 
         self._box = toga.Box(
-            children=[switch_box, split],
+            children=[self._switch_box, self._split],
             style=toga.style.Pack(direction="column"),
         )
 
@@ -28,4 +28,17 @@ class MainWindowCtrl:
 
     @property
     def widget(self) -> toga.Widget:
+        self._box = toga.Box(
+            children=[self._switch_box, self._split],
+            style=toga.style.Pack(direction="column"),
+        )
         return self._box
+
+    def _on_switch_change(self, switch: toga.Switch, **kwargs):
+        self._box.clear()
+
+        if switch.value:
+            self._box.add(self._switch_box, self._split)
+        else:
+            self._box.add(self._switch_box, self._messages_view)
+

59e60f267f79024714dea8833d6f0fc9fdb1914a
alexjuda <alexanderjuda@gmail.com>
2024-08-04 22:14:06 +0200

    Add unit test for loading new batches

--- tests/unit/toga/main_window/test_vm.py
+++ tests/unit/toga/main_window/test_vm.py
@@ -1,8 +1,10 @@
 import asyncio
 from unittest.mock import create_autospec
 
+from pytest import fixture
 from pulsar_viewer.toga.main_window._vm import MainWindowVM
-from pulsar_viewer.pulsar import PulsarPoller
+from pulsar_viewer.toga.main_window._structs import MessageRow
+from pulsar_viewer.pulsar import PulsarPoller, Message
 
 from ._async_mocker import AsyncMocker, trigger_coro
 
@@ -16,24 +18,79 @@ class TestMainWindowVM:
         assert vm._delegate is None
         assert isinstance(vm._poller, PulsarPoller)
 
-    @staticmethod
-    def test_polling_loop():
-        # Given
-        poller_mock = create_autospec(PulsarPoller)
-        vm = MainWindowVM(poller=poller_mock)
+    class TestPollingLoop:
+        @fixture
+        @staticmethod
+        def poller_mock():
+            return create_autospec(PulsarPoller)
 
-        delegate_spy = create_autospec(MainWindowVM.Delegate)
-        vm.register_delegate(delegate_spy)
+        @fixture
+        @staticmethod
+        def vm(poller_mock):
+            return MainWindowVM(poller=poller_mock)
 
-        def _sleep_callback():
-            vm._should_continue_polling = False
+        @fixture
+        @staticmethod
+        def delegate_spy(vm):
+            delegate_spy = create_autospec(MainWindowVM.Delegate)
+            vm.register_delegate(delegate_spy)
+            return delegate_spy
 
-        sleep_mocker = AsyncMocker(asyncio, "sleep", on_await=_sleep_callback)
+        @staticmethod
+        def test_empty_data(vm, delegate_spy):
+            # Given
+            def _sleep_callback():
+                vm._should_continue_polling = False
 
-        with sleep_mocker:
-            # When
-            coro = vm.polling_loop()
-            trigger_coro(coro)
+            sleep_mocker = AsyncMocker(asyncio, "sleep", on_await=_sleep_callback)
 
-        # Then
-        delegate_spy.append_rows.assert_called_once()
+            with sleep_mocker:
+                # When
+                coro = vm.polling_loop()
+                trigger_coro(coro)
+
+            # Then
+            delegate_spy.append_rows.assert_called_once()
+            delegate_spy.append_rows.assert_called_with([])
+
+        @staticmethod
+        def test_batches(poller_mock, vm, delegate_spy):
+            # Given
+            sleep_counter = 0
+            poller_mock.read_new_batch.return_value = [
+                Message(payload=b"m1"),
+                Message(payload=b"m2"),
+            ]
+
+            def _sleep_callback():
+                nonlocal sleep_counter
+
+                if sleep_counter == 0:
+                    poller_mock.read_new_batch.return_value = [
+                        Message(payload=b"m3"),
+                        Message(payload=b"m4"),
+                    ]
+                else:
+                    vm._should_continue_polling = False
+
+                sleep_counter += 1
+
+            sleep_mocker = AsyncMocker(asyncio, "sleep", on_await=_sleep_callback)
+
+            with sleep_mocker:
+                # When
+                coro = vm.polling_loop()
+                trigger_coro(coro)
+
+            # Then
+            assert (
+                len(delegate_spy.append_rows.call_args_list) == 2
+            ), "Invalid number of batches"
+
+            batch1: list[MessageRow] = delegate_spy.append_rows.call_args_list[0].args[0]
+            assert batch1[0].subtitle == "m1"
+            assert batch1[1].subtitle == "m2"
+
+            batch2: list[MessageRow] = delegate_spy.append_rows.call_args_list[1].args[0]
+            assert batch2[0].subtitle == "m3"
+            assert batch2[1].subtitle == "m4"

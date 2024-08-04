08daca2216134611d925deebbefeb95a5de12b34
alexjuda <alexanderjuda@gmail.com>
2024-08-04 20:43:31 +0200

    Add plumbing with VM & delegate

--- src/pulsar_viewer/toga/main_window/_ctrl.py
+++ src/pulsar_viewer/toga/main_window/_ctrl.py
@@ -2,43 +2,25 @@ import toga
 import toga.style
 
 from ._vm import MainWindowVM
+from ._structs import Message
 
 
 class MainWindowCtrl:
     def __init__(self, vm: MainWindowVM):
         self._vm = vm
 
-        switch = toga.Switch(text="Show full message", on_change=self._on_switch_change)
-        # Wrapping the switch in a box makes the control and the label clumped
-        # together instead of stretched to till the container.
-        self._switch_box = toga.Box(children=[switch])
-
-        self._messages_view = toga.Label("Messages\nwill\nshow up here.")
-        self._message_details_view = toga.Label("This will contain\nthe message at its full length.")
-        self._split = toga.SplitContainer(content=(self._messages_view, self._message_details_view))
-
-        self._box = toga.Box(
-            children=[self._switch_box, self._split],
-            style=toga.style.Pack(direction="column"),
-        )
-
     @classmethod
     def standard(cls):
-        return cls(vm=MainWindowVM.standard())
+        vm = MainWindowVM.standard()
+        ctrl = cls(vm=vm)
+        vm.register_delegate(ctrl)
+        return ctrl
 
     @property
     def widget(self) -> toga.Widget:
-        self._box = toga.Box(
-            children=[self._switch_box, self._split],
-            style=toga.style.Pack(direction="column"),
-        )
-        return self._box
+        messages_view = toga.Label("Messages\nwill\nshow up here.")
+        return messages_view
 
-    def _on_switch_change(self, switch: toga.Switch, **kwargs):
-        self._box.clear()
+    def prepend_messages(self, messages: list[Message]): ...
 
-        if switch.value:
-            self._box.add(self._switch_box, self._split)
-        else:
-            self._box.add(self._switch_box, self._messages_view)
-
+    def append_messages(self, messages: list[Message]): ...
--- src/pulsar_viewer/toga/main_window/_vm.py
+++ src/pulsar_viewer/toga/main_window/_vm.py
@@ -1,8 +1,22 @@
+from typing import Protocol
+from ._structs import Message
+
+
 class MainWindowVM:
     """
     View Model for the main window.
     """
 
+    class Delegate(Protocol):
+        def prepend_messages(self, messages: list[Message]): ...
+
+        def append_messages(self, messages: list[Message]): ...
+
+    _delegate: Delegate | None = None
+
     @classmethod
     def standard(cls):
         return cls()
+
+    def register_delegate(self, delegate: Delegate):
+        self._delegate = delegate
